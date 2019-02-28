86d050ddcf0825efd8446ede568f0523149a07e8
liorsh <liorsh@shenkar.ac.il>
2019-02-28 16:47:56 +0200

    favicon

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
-'use strict'
-const express = require('express')
-const bodyParser = require('body-parser')
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+var favicon = require("serve-favicon");
+var path = require("path");
 
-module.exports = function main (options, cb) {
+module.exports = function main(options, cb) {
   // Set default options
-  const ready = cb || function () {}
-  const opts = Object.assign({
-    // Default options
-  }, options)
+  const ready = cb || function() {};
+  const opts = Object.assign(
+    {
+      // Default options
+    },
+    options
+  );
 
   // Server state
-  let server
-  let serverStarted = false
-  let serverClosing = false
+  let server;
+  let serverStarted = false;
+  let serverClosing = false;
 
   // Setup error handling
-  function unhandledError (err) {
-	  // Log the errors
-	  console.error(err)
+  function unhandledError(err) {
+    // Log the errors
+    console.error(err);
 
     // Only clean up once
     if (serverClosing) {
-      return
+      return;
     }
-    serverClosing = true
+    serverClosing = true;
 
     // If server has started, close it down
     if (serverStarted) {
-      server.close(function () {
-        process.exit(1)
-      })
+      server.close(function() {
+        process.exit(1);
+      });
     }
   }
-  process.on('uncaughtException', unhandledError)
-  process.on('unhandledRejection', unhandledError)
+  process.on("uncaughtException", unhandledError);
+  process.on("unhandledRejection", unhandledError);
 
   // Create the express app
-  const app = express()
+  const app = express();
 
   // Common middleware
   // app.use(/* ... */)
-  app.use(bodyParser.json())
+  app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+  app.use(bodyParser.json());
 
   // Register routes
   // @NOTE: require here because this ensures that even syntax errors
@@ -48,43 +54,49 @@ module.exports = function main (options, cb) {
   // Alternativly, you could setup external log handling for startup
   // errors and handle them outside the node process.  I find this is
   // better because it works out of the box even in local development.
-  require('./routes')(app, opts)
+  require("./routes")(app, opts);
 
   // Common error handlers
-  app.use(function fourOhFourHandler (req, res) {
+  app.use(function fourOhFourHandler(req, res) {
     res.status(404).json({
-      messages: [{
-        code: 'NotFound',
-        message: `Route not found: ${req.url}`,
-        level: 'warning'
-      }]
-    })
-  })
-  app.use(function fiveHundredHandler (err, req, res, next) {
-    console.error(err)
+      messages: [
+        {
+          code: "NotFound",
+          message: `Route not found: ${req.url}`,
+          level: "warning"
+        }
+      ]
+    });
+  });
+  app.use(function fiveHundredHandler(err, req, res, next) {
+    console.error(err);
     res.status(500).json({
-      messages: [{
-        code: err.code || 'InternalServerError',
-        message: err.message,
-        level: 'error'
-      }]
-    })
-  })
+      messages: [
+        {
+          code: err.code || "InternalServerError",
+          message: err.message,
+          level: "error"
+        }
+      ]
+    });
+  });
 
   // Start server
-  server = app.listen(opts.port, opts.host, function (err) {
+  server = app.listen(opts.port, opts.host, function(err) {
     if (err) {
-      return ready(err, app, server)
+      return ready(err, app, server);
     }
 
     // If some other error means we should close
     if (serverClosing) {
-      return ready(new Error('Server was closed before it could start'))
+      return ready(new Error("Server was closed before it could start"));
     }
 
-    serverStarted = true
-    const addr = server.address()
-    console.log(`Started at ${opts.host || addr.host || 'localhost'}:${addr.port}`)
-    ready(err, app, server)
-  })
-}
+    serverStarted = true;
+    const addr = server.address();
+    console.log(
+      `Started at ${opts.host || addr.host || "localhost"}:${addr.port}`
+    );
+    ready(err, app, server);
+  });
+};
